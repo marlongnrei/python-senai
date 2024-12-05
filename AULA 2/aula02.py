a34454d4748ce39fbedc77f5373a4acd244311df
marlongnrei <leitemarlon190@gmail.com>
2024-12-05 11:29:05 -0300

    aula sobre palavras variáveis.

---
+++ AULA 2/aula02.py
@@ -0,0 +1,9 @@
+print('Good morning!!')
+nome_usuario = 'Marlon' #tipo -> str = string 
+idade_usuario = 18 #tipo -> int = integer 
+altura_usuario = 1.63 #tipo -> float = flutuante 
+maior_idade = True #tipo -> bool = boolean
+print('seja bem-vindo', nome_usuario)
+print('sua idade é', idade_usuario, 'anos')
+print('sua altura', altura_usuario)
+print('maior de idade>', maior_idade)
